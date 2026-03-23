3e9f93129a7462f4e6fb9d5d3b873c27d8a92c68
Nikhil <nikhil.golla@philips.com>
2026-03-23 11:32:44 +0530

    fixed signup issue

--- frontend-react/src/main.jsx
+++ frontend-react/src/main.jsx
@@ -1,14 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Authenticator } from "@aws-amplify/ui-react";
+import { signUp } from "aws-amplify/auth";
 import "@aws-amplify/ui-react/styles.css";
 import App from "./App";
 import "./styles.css";
 import "./awsConfig";
 
+const authServices = {
+  async handleSignUp(input) {
+    const username = input?.username || "";
+    const password = input?.password || "";
+    const options = input?.options || {};
+    const attrs = options?.userAttributes || {};
+    const formattedName = attrs["name.formatted"] || attrs.name || username;
+
+    return signUp({
+      username,
+      password,
+      options: {
+        ...options,
+        userAttributes: {
+          ...attrs,
+          "name.formatted": formattedName,
+        },
+      },
+    });
+  },
+};
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Authenticator>
+    <Authenticator services={authServices}>
       {({ signOut, user }) => <App signOut={signOut} user={user} />}
     </Authenticator>
   </React.StrictMode>
